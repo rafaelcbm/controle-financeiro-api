fa5abd7e6dc375f4ba8bfbbd481579685b25b797
Rafael Correia <rafaelcbm@gmail.com>
2024-03-01 20:04:39 -0300

    refatoração em pacote

--- src/main/java/br/com/controle/financeiro/repositories/LancamentoRepository.java
+++ src/main/java/br/com/controle/financeiro/repositories/LancamentoRepository.java
@@ -13,7 +13,7 @@ public interface LancamentoRepository extends JpaRepository<Lancamento, String> 
     @Query("SELECT l FROM Lancamento l WHERE l.conta.user.login = :userLogin")
     List<Lancamento> findLancamentosByUser(@Param("userLogin") String userLogin);
 
-    @Query("SELECT new com.example.auth.repositories.dto.LancamentoCompletoDTO(l.id, l.nome, c.nome, cat.nome, l.data, l.valor, l.pago) FROM Lancamento l JOIN l.conta c JOIN l.categoria cat WHERE l.conta.user.login = :userLogin")
+    @Query("SELECT new br.com.controle.financeiro.repositories.dto.LancamentoCompletoDTO(l.id, l.nome, c.nome, cat.nome, l.data, l.valor, l.pago) FROM Lancamento l JOIN l.conta c JOIN l.categoria cat WHERE l.conta.user.login = :userLogin")
     List<LancamentoCompletoDTO> findLancamentosCompletosByUser(@Param("userLogin") String userLogin);
 
 }
