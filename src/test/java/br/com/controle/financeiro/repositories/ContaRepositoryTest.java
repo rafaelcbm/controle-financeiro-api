54954f7f3f1eede1ed9dc783697779f35c02835f
Rafael Correia <rafaelcbm@gmail.com>
2024-03-17 14:00:00 -0300

    teste de repositório de contas

---
+++ src/test/java/br/com/controle/financeiro/repositories/ContaRepositoryTest.java
@@ -0,0 +1,47 @@
+package br.com.controle.financeiro.repositories;
+
+import br.com.controle.financeiro.domain.Conta;
+import br.com.controle.financeiro.domain.user.User;
+import br.com.controle.financeiro.domain.user.UserRole;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class ContaRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ContaRepository contaRepository;
+
+    @Test
+    public void deveObterAsContasDoUsuario() {
+
+        // Arrange
+        String userLogin="joao@teste.com";
+        String userPassword="senha_do_joao";
+        var user = User.builder().login(userLogin).password(userPassword).role(UserRole.ADMIN).build();
+
+        String nomeContaCorrente = "Conta Corrente";
+        Conta contaCorrente = Conta.builder().nome(nomeContaCorrente).user(user).build();
+        String nomeCartaoCredito = "Cartão Crédito";
+        Conta contaCartaoCredito = Conta.builder().nome(nomeCartaoCredito).user(user).build();
+
+        userRepository.save(user);
+        contaRepository.save(contaCorrente);
+        contaRepository.save(contaCartaoCredito);
+
+        // Act
+        List<Conta> contasUsuario = contaRepository.findAllContasByUserLogin(userLogin);
+
+        // Assert
+        Assertions.assertEquals(2, contasUsuario.size());
+    }
+}
