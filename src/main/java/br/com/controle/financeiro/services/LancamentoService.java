f1e47d271fb399ad98acda1e8b73ca2eca8218da
Rafael Correia <rafaelcbm@gmail.com>
2024-05-02 00:54:33 -0300

    correção em validação em atualização de lancamentos

--- src/main/java/br/com/controle/financeiro/services/LancamentoService.java
+++ src/main/java/br/com/controle/financeiro/services/LancamentoService.java
@@ -18,158 +18,156 @@ import java.util.List;
 @Service
 public class LancamentoService {
 
-    @Autowired
-    private BigDecimal valorMaximoLancamento;
+	@Autowired
+	private BigDecimal valorMaximoLancamento;
 
-    private final LancamentoRepository lancamentoRepository;
+	private final LancamentoRepository lancamentoRepository;
 
-    private final ValidacaoDadosUsuarioService validacaoDadosUsuarioService;
+	private final ValidacaoDadosUsuarioService validacaoDadosUsuarioService;
 
-    public LancamentoService(LancamentoRepository lancamentoRepository, ValidacaoDadosUsuarioService validacaoDadosUsuarioService) {
+	public LancamentoService(LancamentoRepository lancamentoRepository,
+			ValidacaoDadosUsuarioService validacaoDadosUsuarioService) {
 
-        this.lancamentoRepository = lancamentoRepository;
-        this.validacaoDadosUsuarioService = validacaoDadosUsuarioService;
-    }
+		this.lancamentoRepository = lancamentoRepository;
+		this.validacaoDadosUsuarioService = validacaoDadosUsuarioService;
+	}
 
-    public List<Lancamento> obterTodosLancamentos(String userLogin) {
-        return lancamentoRepository.findLancamentosByUsuario(userLogin);
-    }
+	public List<Lancamento> obterTodosLancamentos(String userLogin) {
+		return lancamentoRepository.findLancamentosByUsuario(userLogin);
+	}
 
-    public List<LancamentoCompletoDTO> obterTodosLancamentosCompletos(String userLogin) {
-        return lancamentoRepository.findLancamentosCompletosByUsuario(userLogin);
-    }
+	public List<LancamentoCompletoDTO> obterTodosLancamentosCompletos(String userLogin) {
+		return lancamentoRepository.findLancamentosCompletosByUsuario(userLogin);
+	}
 
-    public Lancamento obterLancamentoPorId(String idLancamento, String userLogin) {
+	public Lancamento obterLancamentoPorId(String idLancamento, String userLogin) {
 
-        validacaoDadosUsuarioService.validarLancamentoDoUsuarioLogado(idLancamento, userLogin);
+		validacaoDadosUsuarioService.validarLancamentoDoUsuarioLogado(idLancamento, userLogin);
 
-        return lancamentoRepository.findById(idLancamento).orElseThrow();
-    }
+		return lancamentoRepository.findById(idLancamento).orElseThrow();
+	}
 
-    public List<Lancamento> obterLancamentosPorCompetencia(Integer competencia, String userLogin) {
+	public List<Lancamento> obterLancamentosPorCompetencia(Integer competencia, String userLogin) {
 
-        List<Lancamento> todosLancamentos = lancamentoRepository.findLancamentosByUsuario(userLogin);
+		List<Lancamento> todosLancamentos = lancamentoRepository.findLancamentosByUsuario(userLogin);
 
-        int ano = competencia / 100;
-        int mes = competencia % 100;
+		int ano = competencia / 100;
+		int mes = competencia % 100;
 
-        if (ano < 1000 || ano > 9999) {
-            throw new NegocioException("Ano da competência informada inválido!");
-        }
+		if (ano < 1000 || ano > 9999) {
+			throw new NegocioException("Ano da competência informada inválido!");
+		}
 
-        if (mes < 1 || mes > 12) {
-            throw new NegocioException("Mês da competência informada inválido!");
-        }
+		if (mes < 1 || mes > 12) {
+			throw new NegocioException("Mês da competência informada inválido!");
+		}
 
-        List<Lancamento> lancamentosCompetencia = todosLancamentos.stream()
-                .filter(lancamento -> lancamento.getData().getYear() == ano &&
-                        lancamento.getData().getMonthValue() == mes)
-                .toList();
+		List<Lancamento> lancamentosCompetencia = todosLancamentos.stream().filter(
+				lancamento -> lancamento.getData().getYear() == ano && lancamento.getData().getMonthValue() == mes)
+				.toList();
 
-        return lancamentosCompetencia;
-    }
+		return lancamentosCompetencia;
+	}
 
-    @Transactional
-    public Lancamento criarLancamento(LancamentoRequestDTO lancamentoDTO, String userLogin) {
+	@Transactional
+	public Lancamento criarLancamento(LancamentoRequestDTO lancamentoDTO, String userLogin) {
 
-        validacaoDadosUsuarioService.validarContaDoUsuarioLogado(lancamentoDTO.idConta(), userLogin);
-        validacaoDadosUsuarioService.validarCategoriaDoUsuarioLogado(lancamentoDTO.idCategoria(), userLogin);
+		validacaoDadosUsuarioService.validarContaDoUsuarioLogado(lancamentoDTO.idConta(), userLogin);
+		validacaoDadosUsuarioService.validarCategoriaDoUsuarioLogado(lancamentoDTO.idCategoria(), userLogin);
 
-        if (lancamentoDTO.valor().compareTo(BigDecimal.ZERO) <= 0) {
-            throw new NegocioException("Valor do lançamento informado deve ser maior que 0!");
-        }
+		if (lancamentoDTO.valor().compareTo(BigDecimal.ZERO) <= 0) {
+			throw new NegocioException("Valor do lançamento informado deve ser maior que 0!");
+		}
 
-        if (lancamentoDTO.valor().compareTo(valorMaximoLancamento) > 0) {
-            throw new NegocioException("Valor do lançamento informado não deve ser superior a " + valorMaximoLancamento.toString() + "!");
-        }
+		if (lancamentoDTO.valor().compareTo(valorMaximoLancamento) > 0) {
+			throw new NegocioException(
+					"Valor do lançamento informado não deve ser superior a " + valorMaximoLancamento.toString() + "!");
+		}
 
-        List<Lancamento> lancamentos = lancamentoRepository.findLancamentosByUsuario(userLogin);
-        validarLancamentoComMesmoNomeData(lancamentoDTO.nome(), lancamentoDTO.parseDate(), lancamentos);
+		List<Lancamento> lancamentos = lancamentoRepository.findLancamentosByUsuario(userLogin);
+		validarLancamentoComMesmoNomeData(null, lancamentoDTO.nome(), lancamentoDTO.parseDate(), lancamentos);
 
-        Lancamento lancamento = Lancamento.builder()
-                .nome(lancamentoDTO.nome())
-                .conta(Conta.builder().id(lancamentoDTO.idConta()).build())
-                .categoria(Categoria.builder().id(lancamentoDTO.idCategoria()).build())
-                .data(lancamentoDTO.parseDate())
-                .valor(lancamentoDTO.valor())
-                .pago(lancamentoDTO.pago())
-                .build();
+		Lancamento lancamento = Lancamento.builder().nome(lancamentoDTO.nome())
+				.conta(Conta.builder().id(lancamentoDTO.idConta()).build())
+				.categoria(Categoria.builder().id(lancamentoDTO.idCategoria()).build()).data(lancamentoDTO.parseDate())
+				.valor(lancamentoDTO.valor()).pago(lancamentoDTO.pago()).build();
 
-        return lancamentoRepository.save(lancamento);
-    }
+		return lancamentoRepository.save(lancamento);
+	}
 
-    @Transactional
-    public Lancamento atualizarLancamento(String idLancamento, LancamentoRequestDTO lancamentoDTO, String userLogin) {
+	@Transactional
+	public Lancamento atualizarLancamento(String idLancamento, LancamentoRequestDTO lancamentoDTO, String userLogin) {
 
-        validacaoDadosUsuarioService.validarContaDoUsuarioLogado(lancamentoDTO.idConta(), userLogin);
-        validacaoDadosUsuarioService.validarCategoriaDoUsuarioLogado(lancamentoDTO.idCategoria(), userLogin);
-        validacaoDadosUsuarioService.validarLancamentoDoUsuarioLogado(idLancamento, userLogin);
+		validacaoDadosUsuarioService.validarContaDoUsuarioLogado(lancamentoDTO.idConta(), userLogin);
+		validacaoDadosUsuarioService.validarCategoriaDoUsuarioLogado(lancamentoDTO.idCategoria(), userLogin);
+		validacaoDadosUsuarioService.validarLancamentoDoUsuarioLogado(idLancamento, userLogin);
 
-        if (lancamentoDTO.valor().compareTo(BigDecimal.ZERO) <= 0) {
-            throw new NegocioException("Valor do lançamento informado deve ser maior que 0!");
-        }
-        if (lancamentoDTO.valor().compareTo(valorMaximoLancamento) > 0) {
-            throw new NegocioException("Valor do lançamento informado não deve ser superior a " + valorMaximoLancamento.toString() + "!");
-        }
+		if (lancamentoDTO.valor().compareTo(BigDecimal.ZERO) <= 0) {
+			throw new NegocioException("Valor do lançamento informado deve ser maior que 0!");
+		}
+		if (lancamentoDTO.valor().compareTo(valorMaximoLancamento) > 0) {
+			throw new NegocioException(
+					"Valor do lançamento informado não deve ser superior a " + valorMaximoLancamento.toString() + "!");
+		}
 
-        List<Lancamento> lancamentos = lancamentoRepository.findLancamentosByUsuario(userLogin);
-        validarLancamentoComMesmoNomeData(lancamentoDTO.nome(), lancamentoDTO.parseDate(), lancamentos);
+		List<Lancamento> lancamentos = lancamentoRepository.findLancamentosByUsuario(userLogin);
+		validarLancamentoComMesmoNomeData(idLancamento, lancamentoDTO.nome(), lancamentoDTO.parseDate(), lancamentos);
 
-        Lancamento lancamento = lancamentoRepository.findById(idLancamento).orElseThrow();
+		Lancamento lancamento = lancamentoRepository.findById(idLancamento).orElseThrow();
 
-        lancamento.setNome(lancamentoDTO.nome());
-        lancamento.setConta(Conta.builder().id(lancamentoDTO.idConta()).build());
-        lancamento.setCategoria(Categoria.builder().id(lancamentoDTO.idCategoria()).build());
-        lancamento.setData(lancamentoDTO.parseDate());
-        lancamento.setValor(lancamentoDTO.valor());
-        lancamento.setPago(lancamentoDTO.pago());
+		lancamento.setNome(lancamentoDTO.nome());
+		lancamento.setConta(Conta.builder().id(lancamentoDTO.idConta()).build());
+		lancamento.setCategoria(Categoria.builder().id(lancamentoDTO.idCategoria()).build());
+		lancamento.setData(lancamentoDTO.parseDate());
+		lancamento.setValor(lancamentoDTO.valor());
+		lancamento.setPago(lancamentoDTO.pago());
 
-        return lancamentoRepository.save(lancamento);
-    }
+		return lancamentoRepository.save(lancamento);
+	}
 
-    protected void validarLancamentoComMesmoNomeData(String nomeLancamento, LocalDate dataLancamento, List<Lancamento> lancamentos) {
+	protected void validarLancamentoComMesmoNomeData(String idLancamento,  String nomeLancamento, LocalDate dataLancamento,
+			List<Lancamento> lancamentos) {
 
-        boolean possuiLancamentoComMesmoNomeData = lancamentos.stream()
-                .anyMatch(l -> l.getNome().equals(nomeLancamento) &&
-                        l.getData().equals(dataLancamento)
-                );
-        if (possuiLancamentoComMesmoNomeData) {
-            throw new NegocioException("Lançamento com nome e data informados já existente.");
-        }
-    }
+		boolean possuiOutroLancamentoComMesmoNomeData = lancamentos.stream()
+				.anyMatch(l -> !l.getId().equals(idLancamento) &&
+						l.getNome().equals(nomeLancamento) && l.getData().equals(dataLancamento));
+		if (possuiOutroLancamentoComMesmoNomeData) {
+			throw new NegocioException("Lançamento com nome e data informados já existente.");
+		}
+	}
 
-    @Transactional
-    public Lancamento atualizarLancamentoComoPago(String idLancamento, String userLogin) {
+	@Transactional
+	public Lancamento atualizarLancamentoComoPago(String idLancamento, String userLogin) {
 
-        validacaoDadosUsuarioService.validarLancamentoDoUsuarioLogado(idLancamento, userLogin);
-        Lancamento lancamento = lancamentoRepository.findById(idLancamento).orElseThrow();
+		validacaoDadosUsuarioService.validarLancamentoDoUsuarioLogado(idLancamento, userLogin);
+		Lancamento lancamento = lancamentoRepository.findById(idLancamento).orElseThrow();
 
-        if (lancamento.isPago()) {
-            throw new NegocioException("Lançamento já está pago!");
-        } else {
-            lancamento.setPago(true);
-            return lancamentoRepository.save(lancamento);
-        }
-    }
+		if (lancamento.isPago()) {
+			throw new NegocioException("Lançamento já está pago!");
+		} else {
+			lancamento.setPago(true);
+			return lancamentoRepository.save(lancamento);
+		}
+	}
 
-    @Transactional
-    public Lancamento atualizarLancamentoComoNaoPago(String idLancamento, String userLogin) {
+	@Transactional
+	public Lancamento atualizarLancamentoComoNaoPago(String idLancamento, String userLogin) {
 
-        validacaoDadosUsuarioService.validarLancamentoDoUsuarioLogado(idLancamento, userLogin);
-        Lancamento lancamento = lancamentoRepository.findById(idLancamento).orElseThrow();
+		validacaoDadosUsuarioService.validarLancamentoDoUsuarioLogado(idLancamento, userLogin);
+		Lancamento lancamento = lancamentoRepository.findById(idLancamento).orElseThrow();
 
-        if (!lancamento.isPago()) {
-            throw new NegocioException("Lançamento já não está pago!");
-        } else {
-            lancamento.setPago(false);
-            return lancamentoRepository.save(lancamento);
-        }
-    }
+		if (!lancamento.isPago()) {
+			throw new NegocioException("Lançamento já não está pago!");
+		} else {
+			lancamento.setPago(false);
+			return lancamentoRepository.save(lancamento);
+		}
+	}
 
-    @Transactional
-    public void deletarLancamento(String idLancamento, String userLogin) {
+	@Transactional
+	public void deletarLancamento(String idLancamento, String userLogin) {
 
-        validacaoDadosUsuarioService.validarLancamentoDoUsuarioLogado(idLancamento, userLogin);
-        lancamentoRepository.deleteById(idLancamento);
-    }
+		validacaoDadosUsuarioService.validarLancamentoDoUsuarioLogado(idLancamento, userLogin);
+		lancamentoRepository.deleteById(idLancamento);
+	}
 }
